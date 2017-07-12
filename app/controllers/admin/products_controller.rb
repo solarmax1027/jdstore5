b33315aa03cb0a5f760439c3d0d710eb31050860
solarmax <solarmax1027@gmail.com>
2017-07-12 21:48:24 +0800

    only admin can access backed panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
+ before_action :authenticate_user!
+
+ before_action :admin_required
+
+  layout "admin"
 
   def index
     @product = Product.all
